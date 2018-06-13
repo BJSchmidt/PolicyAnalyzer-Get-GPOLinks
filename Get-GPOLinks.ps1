4c6d5f7e21aad32817c404e2f3c6168686ae4fb8
Ben Schmidt <bschmidt@corus360.com>
2018-06-13 09:40:49 -0600

    Import and Export working Colors are alternating correctly Progress bar works GPOs we cannot report on are exported into a new tab

--- Get-GPOLinks.ps1
+++ Get-GPOLinks.ps1
@@ -7,14 +7,15 @@ Process:
     Import excel document
     Parse out GPO Names from the "baudette-mn_PolicyRules %newline% GPO" field (column K)
     Get XML Report for all the gpos, once.
-
     Parse XML for Name, Links, Enforced, GUID
     Export to CSV.
-
-TODO: Prompt for TargetDomain (~ Line 30)
 #>
 
 #requires -module ImportExcel
+
+$TargetDomain = ""
+$InputFile = ""
+$OutputFile = ".\test.xlsx"
 
 function Get-FileName($initialDirectory) {
     $initialDirectory = $PSscriptroot
@@ -35,78 +36,89 @@ function Get-GPOLinks() {
         $TargetDomain,
 
         [Parameter(Mandatory)]
-        [Alias("Input","File")]
-        [string]$InputFile
+        [Alias("Input")]
+        [string]$InputFile,
 
-        # [array]$GPONames
+        [Parameter(Mandatory)]
+        [Alias("Output")]
+        [string]$OutputFile
     )
 
-    Process{        
+    Process {
         
-        Write-Output "TargetDomain: $TargetDomain"
-        # Write-Output "GPOs: $GPONames"
+        #Vars:
+        $errorCountGPOReport = 0
+        $GPOReportErrors = @()
+
+        Write-Output "`nTargetDomain: $TargetDomain"
         Write-Output "Input File: $InputFile"
-        
+        Write-Output "Output File: $OutputFile"
+       
         $confirmation = Read-Host -Prompt "Continue? (Y)es / Press any other key to exit"
         if ($confirmation -ne 'y' -and $confirmation -ne 'Y') {
             exit
         }
-        
+
         $Excel = Import-Excel $InputFile
-        
-################################################        
+
         # Get all Unique GPO Names:
         $allGPONames = $($($excel | Select-Object -ExpandProperty "*PolicyRules*GPO").split("`n")).trim() | Select-Object -Unique
 
+        Write-Host "`nGetting GPO Reports for $($allGPONames.count) GPOs:"
         #Then get their XML Reports & store them:
+
+        $count = 0
         $gpoXMLArray = foreach ($gpo in $allGPONames) {
-            try{
-                [XML]$(Get-GPOReport -Name $gpo -Domain $targetDomain -ReportType XML)
-                Write-Host "Got GPO Report for $gpo"
-            }
-            catch{
-                Write-Host "Could not get GPO Report for $gpo"
+            $count++
+            Write-Progress -Id 1 -Activity "Getting GPO Reports" -Status "Getting GPO Report $count of $($allGPONames.count): $gpo" -PercentComplete $([math]::Round(($count/$allGPONames.count)*100)) -CurrentOperation "$([math]::Round(($count/$allGPONames.count)*100))% Complete"
+            try {
+                [XML]$(Get-GPOReport -Name $gpo -Domain $targetDomain -ReportType XML -ErrorAction Stop)
+                # Write-Host "Got GPO Report for $gpo"
+            } catch {
+                Write-Host "Could not get GPO Report for $gpo. Was it removed since the backup was created?"
+                $errorCountGPOReport++
+                $GPOReportErrors += $gpo
             }
         }
+        Write-Progress -Id 1 -Completed -Activity "Getting GPO Reports"
+        if ($errorCountGPOReport) {
+            Write-Host "Could not get $errorCountGPOReport / $($allGPONames.count) GPO Reports"
+        }
 
-        # Then loop through each row in excel, so we can generate output in the same order as the input file.
-
-
-
-
-
-
-
-        
-###########################
-        #Get GPO Names:
-        $currRow = 0
+        Write-Host "Processing GPOs"
+               
+        # Loop through each row in $Excel, so we can generate output in the same order as the input file.
+        $currRow = 2
         $results = foreach ($row in $Excel) {
-            $currRow++
-            $gpoNames = $row | Select-Object -ExpandProperty "*PolicyRules*GPO"
-            # Write-Host $gpoNames
-            $gpoXMLArray = foreach ($gpo in $gpoNames.split("`n")) {
-                try{
-                    [XML]$(Get-GPOReport -Name $gpo -Domain $targetDomain -ReportType XML)
-                    Write-Host "Got GPO Report for $gpo"
-                }
-                catch{
-                    Write-Host "Could not get GPO Report for $gpo"
-                }
-            }
+            # Get GPOs in this cell:
+            $thisCellsGPOs = $($($row | Select-Object -ExpandProperty "*PolicyRules*GPO").split("`n")).trim()
+            # Write-Host "Row: $currRow | GPOs in this cell: $($thisCellsGPOs.count) | $thisCellsGPOs"
             
-            $gpoXMLArray.GPO |
-            Select-Object -Property Name,
-                @{Name="Links (Expand row height to see multiline cells) "; Expression = {[string]::join("`n",($_.LinksTo.SOMPath))}},
-                @{Name="Enabled"; Expression = {[string]::join("`n",($_.LinksTo.Enabled))}},
-                @{Name="Enforced"; Expression = {[string]::join("`n",($_.LinksTo.NoOverride))}},
-                @{Name="GUID"; Expression = {$_.Identifier.Identifier."#Text" }},
-                @{Name="currRow"; Expression = {$currRow % 2}}
+            foreach ($gpo in $thisCellsGPOs) {
+                # Match to an item in $gpoXMLArray, then output that.
+                $gpoXMLArray.GPO | Where-Object {$_.Name -eq $gpo} |
+                Select-Object -Property @{Name="Analyzer Source Row"; Expression = {$currRow}},
+                                                Name,
+                                        @{Name="Enabled"; Expression = {[string]::join("`n",($_.LinksTo.Enabled))}},
+                                        @{Name="Enforced"; Expression = {[string]::join("`n",($_.LinksTo.NoOverride))}},
+                                        @{Name="GUID"; Expression = {$_.Identifier.Identifier."#Text" }},
+                                        # @{Name="currRow"; Expression = {$currRow % 2}},
+                                        @{Name="Links (Expand row height to see multiline cells) "; Expression = {[string]::join("`n",($_.LinksTo.SOMPath))}}
+            }
+            $currRow++        
         }
-        
-        # Export:
+       
+        # Export $Results:
         # Color based on $currRow % 2
-        $results | Export-Excel .\test.xlsx -show -CellStyleSB {
+        Write-Host "Writing Output file $OutputFile"
+
+        # Delete Outputfile if it already exists
+        if (Test-Path $OutputFile)  {
+            Remove-Item $OutputFile
+        }
+
+        #Export Results:
+        $results | Export-Excel $OutputFile -WorkSheetname "GPO Links" -AutoSize -CellStyleSB {
             param (
                 $workSheet,
                 $totalRows,
@@ -114,32 +126,32 @@ function Get-GPOLinks() {
             )
         
             foreach ($row in (2..$totalRows)) {
-                # Set CellStyle (alternating row colors):
-                # if ($row % 2 -eq 0) {
-                #     Set-CellStyle $workSheet $row $LastColumn Solid Gray
-                # } elseif ($row % 2 -eq 1) {
-                #     Set-CellStyle $workSheet $row $LastColumn Solid LightGray
-                # }
-        
-                # Set CellStyle based on column 6
-                if ($workSheet.cells[$row,6].Value -eq 0) {
+                if ($workSheet.cells[$row,1].Value % 2 -eq 0) {
                     Set-CellStyle $workSheet $row $lastColumn Solid Gray
-                } elseif ($workSheet.cells[$row,6].Value -eq 1) {
+                } elseif ($workSheet.cells[$row,1].Value % 2 -eq 1) {
                     Set-CellStyle $workSheet $row $lastColumn Solid LightGray
                 }
             }
         }
+
+        #Export list of gpos that we could not get a report for.
+        if ($GPOReportErrors) { $GPOReportErrors | Export-Excel $OutputFile -WorkSheetname "Missing Reports" -AutoSize -show }
     }
 }
 
-$TargetDomain
-$InputFile
-
 if ($TargetDomain -like "") {
-    #Prompt for input file:
-    $TargetDomain = "baudette-mn.catholichealth.net"
+    #Prompt for TargetDomain:
+    $TargetDomain = Read-Host -Prompt "Enter Target Domain"
 }
 
-$InputFile = Get-FileName($PSScriptRoot)
+if ($InputFile -like "") {
+    #Prompt for InputFile:
+    $InputFile = Get-FileName($PSScriptRoot)
+}
 
-Get-GPOLinks -TargetDomain $TargetDomain -InputFile $inputFile
+if ($OutputFile -like "") {
+    #Prompt for OutputFile:
+    $OutputFile = Read-Host -Prompt "Enter OutputFile path: Ex '.\outputFile.xlsx'"
+}
+
+Get-GPOLinks -TargetDomain $TargetDomain -InputFile $inputFile -outputFile $OutputFile
